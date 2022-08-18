163aefee739476d2830a78ad580fa5b63373b3c6
Chinmoy <nathchinmoy777@gmail.com>
2022-08-18 14:34:16 +0530

    initialized new project and installed deps

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//  import * as serviceWorkerRegistration.register();
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />, document.getElementById('root'));
+  
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
