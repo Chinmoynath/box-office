8433258e7d6fad5a3f7f5437bdc2bd545eca243e
Chinmoy <nathchinmoy777@gmail.com>
2022-08-24 17:09:43 +0530

    deployed the app and start using hash-router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 //  
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -12,9 +12,9 @@ import reportWebVitals from './reportWebVitals';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
  
 );
   
